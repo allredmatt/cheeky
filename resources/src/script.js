090680948c62fccca22f7ec4e8f8152e71d1f8aa
allredmatt <matt@allred.digital>
2025-01-08 16:53:20 +0000

    Add login button

---
+++ resources/src/script.js
@@ -0,0 +1,8 @@
+var buttonAside = document.getElementById("login")
+var infoAside = document.getElementById("logged-in")
+
+
+function toggleHidden () {
+    buttonAside.hidden = true
+    infoAside.hidden = false
+}
